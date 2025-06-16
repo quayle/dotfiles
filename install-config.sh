d65c316aa78aff8b5d7164e2d0a34a48e60538f8
Rafał Długołęcki <rafal@dlugolecki.net.pl>
2025-06-16 21:30:45 +0200

    Fonts

--- install-config.sh
+++ install-config.sh
@@ -2,15 +2,16 @@
 
 DOTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-configs=("alacritty" "foot" "i3" "i3status" "i3blocks" "nvim" "rofi" "sway" "swayidle" "swaylock" "waybar" "wlogout" "wob" "wofi")
+configs=("alacritty" "fontconfig" "foot" "i3" "i3status" "i3blocks" "nvim" "rofi" "sway" "swayidle" "swaylock" "waybar" "wlogout" "wob" "wofi")
 
 for config in ${configs[@]}; do
     if test -L "$HOME/.config/$config"; then
-        echo "Not linking '$config' as link already exists";
+        echo "[-] not linking '$config' as link already exists";
     elif test -d "$HOME/.config/$config"; then
-        echo "Not linking '$config' as directory already exists";
+        echo "[-] Not linking '$config' as directory already exists";
     else 
         ln -s "$DOTDIR/.config/$config" "$HOME/.config/$config";
+        echo "[+] Linking '.config/$config'";
     fi;
 done
 
@@ -20,9 +21,10 @@ done
 
 share="fonts"
 if test -L "$HOME/.local/share/$share"; then
-    echo "Not linking '$share' as link already exists";
+    echo "[-] Not linking '$share' as link already exists";
 elif test -d "$HOME/.local/share/$share"; then
-    echo "Not linking '$share' as directory already exists";
+    echo "[-] Not linking '$share' as directory already exists";
 else
     ln -s "$DOTDIR/.local/share/$share" "$HOME/.local/share/$share";
+    echo "[+] Linking '$config'";
 fi;
