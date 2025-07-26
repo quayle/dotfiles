ca147b14cbba5c13358d97cfc349d98e44da51f9
Rafał Długołęcki <rafal@dlugolecki.net.pl>
2025-07-26 21:07:14 +0200

    nvim (alternatives)

--- install-vim.sh
+++ install-vim.sh
@@ -10,5 +10,13 @@ sudo mv /tmp/nvim /usr/local/bin/nvim
 echo "[ ] Setting alternatives"
 sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/nvim 1 && \
 sudo update-alternatives --set editor /usr/local/bin/nvim
+# Now calling: update-alternatives --config editor, will show local nvim
+
+sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 1 && \
+sudo update-alternatives --set vim /usr/local/bin/nvim
+# Now calling: update-alternatives --config nvim, will show local nvim
+
+# update-alternatives --list vim
+# update-alternatives --remove vim /usr/local/bin/nvim
 
 echo "[ ] Check: .config/nvim/install.sh"
