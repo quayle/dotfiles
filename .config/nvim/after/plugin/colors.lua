45441bf4bd0f06d58036b875d448a9d2b6904ce4
Rafał Długołęcki <rafal@dlugolecki.net.pl>
2026-04-22 08:24:46 +0200

    nvim: theme

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,11 +2,11 @@ function ColorMyPencils(color)
 
     -- color = color or "rose-pine"
     color = color or "gruber-darker"
+    color = "gruber-darker"
     vim.cmd.colorscheme(color)
 
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
